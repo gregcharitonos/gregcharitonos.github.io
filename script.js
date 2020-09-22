7b79ecdd8b316d710de85095609975bc9a94bfcf
Greg Charitonos <2048264+gregcharitonos@users.noreply.github.com>
2020-09-22 11:24:39 +0200

    tweak

--- script.js
+++ script.js
@@ -8,18 +8,17 @@ init = function(){
   let parallax = function(e){
     let dy = null;
     let dx = null;
+    mx = E.card.getBoundingClientRect().width/2;
+    my = E.card.getBoundingClientRect().height/2;
     if(e.type == "mousemove"){
       let x = e.clientX;
       let y = e.clientY;
-      let mx = e.currentTarget.clientWidth/2;
-      let my = e.currentTarget.clientHeight/2;
-      dx = -(mx - x)/mx;
+      dx = -(mx - x)/mx - 2;
       dy = -(my - y)/my;
     } else {
       dx = e.alpha/36; dx = Math.sign(dx) * Math.min(1,Math.abs(dx));
       dy = e.beta/36; dy = Math.sign(dy) * Math.min(1,Math.abs(dy));
     }
-    
     E.card.style.transform = `
       rotateY(${Math.floor(dx*10)}deg)
       rotateX(${Math.floor(dy*10)}deg)
@@ -41,34 +40,7 @@ init = function(){
     `
   }
 
-  E.card.addEventListener("mousemove",parallax,false);
-  window.addEventListener("deviceorientation",parallax,false);
-
-  slowStyles = async function(){
-    let data = await fetch("./style.css").then(d=>{return d.text()});
-    let length = data.length;
-    let index = 0;
-    let style= document.createElement("style");
-    document.head.appendChild(style);
-    update(data,0,style);
-  }
-
-  update = function(data,index,style){
-    style.innerHTML += data[index];
-    E.background.textContent += data[index];
-    if(index < data.length-1){
-      index++;
-      setTimeout(()=>{
-        update(data,index,style);
-      },1000/60)
-    } else {
-      E.background.contentEditable = true;
-      E.background.addEventListener("keyup",e=>{
-        style.innerHTML = E.background.textContent;
-      },false);
-    }
-  }
-
-  slowStyles();
+   E.card.addEventListener("mousemove",parallax,false);
+   window.addEventListener("deviceorientation",parallax,false);
 }
 window.onload = init;
